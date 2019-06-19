58d610468ac29c8d06b3c21060486c7ee243b270
maguilarga <maguporras@hotmail.com>
2019-06-19 18:42:15 -0500

    change

---
+++ PyBank/main.py
@@ -0,0 +1,6 @@
+# Import the necessary dependencies for os.path.join()
+import os
+import csv
+
+# Read in the CSV file
+csv_file = os.path.join("/C/Rice/Data_Analytics/RICEHOU201906DATA1/HW/03-Python/Instructions/PyBank/Resources/","budget_data.csv")
