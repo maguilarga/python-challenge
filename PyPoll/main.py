ff1d8b2dd52680e948db4d9defac36f66c0ae596
maguilarga <maguporras@hotmail.com>
2019-06-21 18:31:56 -0500

    PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,16 @@
-# PyPoll
+# PyPoll ??? How to comment many lines at a time?????
 # Import the necessary dependencies for os.path.join()
 import os
 import csv
 
 ## CHECK VARIABLES *******
 NumVoters = 0
+Cand_List = {}
+Candidate = ""
 
-Revenue = 0
 Change = 0
 SumChange = 0
-Grt_Inc_Dec = ["", 0, "", 0]
+
 
 # Specify path and file name
 # in_filepath = "/C/Rice/Data_Analytics/RICEHOU201906DATA1/HW/03-Python/Instructions/PyPoll/Resources/" ???
@@ -24,47 +25,32 @@ with open(csv_file, newline='') as in_csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
+    # print(csv_header)
 
     # Read each row of data after the header
     for row in csvreader:
         NumVoters += 1
-        
-        Revenue += int(row[1])
-        if NumMonths > 1:
-            Change = int(row[1]) - Change
-            if Change > int(Grt_Inc_Dec[1]):
-                Grt_Inc_Dec[0] = row[0]
-                Grt_Inc_Dec[1] = Change
-            elif Change < int(Grt_Inc_Dec[3]):
-                Grt_Inc_Dec[2] = row[0]
-                Grt_Inc_Dec[3] = Change
+        Candidate = row[2]
+        if Candidate in Cand_List:
+            Cand_List[Candidate] += 1
+        else:
+            Cand_List[Candidate] = 1
 
-            SumChange += Change
-        Change = int(row[1])  
-
-# Specify the file to write to
-csv_file = os.path.join("", "PyBank.out")
-
-# Open the file using "write" mode. Specify the variable to hold the contents
-with open(csv_file, 'w', newline='') as out_csvfile:
-    # new line is NOT working ???
-
-    print("Financial Analysis")
-    out_csvfile.write("Financial Analysis")
-    print("----------------------------")
-    out_csvfile.write("----------------------------")
-    out_line = "Total Months: {:d}".format(NumMonths)
+Winner = ""
+Win_Votes = 0
+print("Election Result")
+print("----------------------------")
+out_line = "Total Votes: {:1,d}".format(NumVoters)
+print(out_line)
+print("----------------------------")
+for cand, votes in Cand_List.items():
+    out_line = "{}: {:1.3f}% ({:,d})".format(cand,(votes/NumVoters)*100,votes)
     print(out_line)
-    out_csvfile.write(out_line)
-    out_line = "Total: ${:1,d}".format(Revenue)
-    print(out_line)
-    out_csvfile.write(out_line)
-    out_line = "Average Change: ${:1,.2f}".format(SumChange/(NumMonths - 1))
-    print(out_line)
-    out_csvfile.write(out_line)
-    out_line = "Greatest Increase in Profits:{} (${:1,.0f})".format(Grt_Inc_Dec[0],Grt_Inc_Dec[1])
-    print(out_line)
-    out_csvfile.write(out_line)
-    out_line = "Greatest Decrease in Profits: {} (${:1,.0f})".format(Grt_Inc_Dec[2],Grt_Inc_Dec[3])
-    print(out_line)
-    out_csvfile.write(out_line)
+    if votes > Win_Votes:
+        Winner = cand
+        Win_Votes = votes
+print("----------------------------")
+print(f"Winner: {Winner}")
+print("----------------------------")
+# print(Cand_List)
+# instructions talk about "each dataset provided", is there extra ones?????
