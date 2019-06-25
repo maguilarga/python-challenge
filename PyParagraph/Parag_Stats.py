fdb7e5b82eeb6bfb851c2861c4842851f5674378
maguilarga <maguporras@hotmail.com>
2019-06-24 21:38:18 -0500

    PyStats

--- PyParagraph/Parag_Stats.py
+++ PyParagraph/Parag_Stats.py
@@ -6,10 +6,44 @@ import re
 # Specify path and file name for input and output
 in_file = os.path.join(".", "analyze_paragrah.txt")
 
+# Initialize variables
+Sentences = 0
+Words = 0
+Letter_Count = 0
+Avg_Letter_Count = 0.0
+Avg_Word_Count = 0.0
 
 # Read in the file
 with open(in_file, 'r') as infile:
 
     # Store all of the text inside a variable
      paragraph = infile.read()
+     parag_list = re.split("(?<=[.!?]) +", paragraph)
+
+     Sentences = len(parag_list)
+     for sent in parag_list:
+         word_list = re.split("\s", sent)
+         Words += len(word_list)
+         for word in word_list:
+             # Last character is a comma
+             if (word[-1:] == ','):
+                 Letter_Count += len(word)
+             else:
+                 Letter_Count += (len(word) -1)
+                
+             # print(str(Letter_Count))
+         
+print ("Paragraph Analysis")
+print ("---------------")
+print(f"Approximate Word Count: {Words}")
+print(f"Approximate Sentence Count: {Sentences}")
+print(f"Average Letter Count: {Letter_Count/Words}")
+print(f"Average Sentence Length: {Words/Sentences}")
+
+             
+             
+
+
+     
+     
 
